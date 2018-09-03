d32cc52f7afe45e203dd326b08900409644f4d62
Babat <babat42@gmail.com>
2018-09-03 21:39:34 +0200

    Add module controller to create a controller in a module

---
+++ src/Controllers/ModuleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Kastra.Core.Business;
+using Kastra.Core.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kastra.Core.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public ModuleDefinitionInfo ModuleDefinition
+        { 
+            get
+            {
+                if(_moduleDefinition == null)
+                {
+                    _moduleDefinition = _viewManager.GetModuleDefsList()
+                                          .SingleOrDefault(m => m.Namespace == this.GetType().Namespace);
+                }
+
+                return _moduleDefinition;
+            }
+        }
+
+        private ModuleDefinitionInfo _moduleDefinition = null;
+
+        private IViewManager _viewManager = null;
+
+
+        public ModuleController(IViewManager viewManager)
+        {
+            _viewManager = viewManager;
+        }
+
+        public override ViewResult View(string viewName)
+        {
+            if(ModuleDefinition == null)
+            {
+                return View(viewName);
+            }
+
+            return View($"{ModuleDefinition.Path}");
+        }
+    }
+}
