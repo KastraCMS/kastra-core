9bba87d447aba457863c3cb6efc2564c27063bfd
Babat <babat42@gmail.com>
2018-09-16 19:14:16 +0200

    Add email service

---
+++ src/Services/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Kastra.Core.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+        void SendEmail(string email, string subject, string message);
+    }
+}
