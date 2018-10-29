2e73197910b8204b1e34538cdf3c2bd0fba76efb
Babat <babat42@gmail.com>
2018-10-29 23:32:27 +0100

    Add dates in user table

--- src/DTO/ApplicationUser.cs
+++ src/DTO/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Kastra.Core.Dto
@@ -6,6 +7,18 @@ namespace Kastra.Core.Dto
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Gets or sets the date when an user is created. 
+        /// </summary>
+        /// <value></value>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when an user is modified.
+        /// </summary>
+        /// <value></value>
+        public DateTime DateModified { get; set; }
+
         /// <summary>
         /// Navigation property for the roles this user belongs to.
         /// </summary>
