9a334a54018bba79687b81abd823f2aaa52e2a79
Babat <babat42@gmail.com>
2019-07-21 19:04:06 +0200

    Add license text

--- src/DTO/ApplicationRole.cs
+++ src/DTO/ApplicationRole.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/KastraCMS/kastra-core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Kastra.Core.Dto
