e278625c14be9724e7d6e8c251e5eff5f3b29e6a
Baptiste Coppéré <baptiste.coppere@netskydev.eu>
2023-02-04 15:24:11 +0100

    feat: update user model

--- src/Kastra.Admin.Core/Models/UserModel.cs
+++ src/Kastra.Admin.Core/Models/UserModel.cs
@@ -4,31 +4,60 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Kastra.Admin.Core.Models
 {
+    /// <summary>
+    /// User model
+    /// </summary>
     public class UserModel
     {
+        /// <summary>
+        /// User Id
+        /// </summary>
         public Guid? Id { get; set; }
 
+        /// <summary>
+        /// User name
+        /// </summary>
         [Required]
         [EmailAddress]
         [StringLength(150)]
         [Display(Name = "username")]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Email
+        /// </summary>
         [EmailAddress]
         [StringLength(150)]
         [Display(Name = "email")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// First name
+        /// </summary>
         [StringLength(150)]
         [Display(Name = "first name")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last name
+        /// </summary>
         [StringLength(150)]
         [Display(Name = "last name")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
 
+        /// <summary>
+        /// If the email address is confirmed
+        /// </summary>
+        public bool EmailConfirmed { get; set; }
+
+        /// <summary>
+        /// Roles list
+        /// </summary>
         public IList<string> Roles { get; set; }
     }
 }
